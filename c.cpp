31ee8848713e6a5aa8f0630d5f35cfd230ae3bc2
sivasathiya <sathyasiva369@gmail.com>
2017-06-28 15:30:29 +0530

    Create c.cpp

---
+++ c.cpp
@@ -0,0 +1,17 @@
+#include <iostream.h>
+
+int main()
+{
+char a;
+cin<<a;
+if((a='a'&&a<='z')||(a='A')&&(a<='Z'))
+{
+cout<<"alphabet";
+}
+    else
+    {
+        cout<<"not";
+    }
+return 0;
+}
+
